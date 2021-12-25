eae8636a3c20cc95857ff7c7c305e8f8bfa47924
你好2007 <2501482523@qq.com>
2021-12-25 23:50:57 +0800

    发布初始化版本0.1.0

--- src/index.js
+++ src/index.js
@@ -2,6 +2,21 @@ function PlugName() { }
 
 PlugName.prototype.afterLoader = function (config, info) {
 
+    if (/node_modules/.test(info.filepath) && /module\.exports/.test(info.source)) {
+        info.source = `
+
+        var module={
+            exports:{}
+        };
+        var exports=module.exports;
+
+        ${info.source}
+
+        export default module.exports;
+
+        `;
+    }
+
     return info;
 };
 
---
+++ test/src/main.js
@@ -0,0 +1,5 @@
+import { isArray } from '@hai2007/tool/type';
+import image2D from 'image2d';
+
+console.log(image2D('#root'));
+console.log(isArray(123));
